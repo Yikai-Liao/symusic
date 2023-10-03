cc6a0ef6ed7fefaf85e249c0cca6179bde6b6205
lzqlzzq <79011287+lzqlzzq@users.noreply.github.com>
2023-10-03 18:30:42 +0800

    Step version to 0.0.5

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="symusic",
-    version="0.0.4",
+    version="0.0.5",
     author="Yikai Liao",
     author_email="lyk-boya@outlook.com",
     description="A high performance MIDI file parser with comprehensible interface.",
