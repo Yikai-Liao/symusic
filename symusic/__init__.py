9214fc98a57c67332ecf3f0ba7bca923e8b253ea
lyk <lyk-boya@outlook.com>
2023-12-05 15:15:01 +0800

    update to v0.1.2

--- symusic/__init__.py
+++ symusic/__init__.py
@@ -1,6 +1,9 @@
 from .factory import *
 
+__version__ = '0.1.2'
+
 __all__ = [
+    
     'TimeUnit', 'Note', 'KeySignature', 'TimeSignature',
     'ControlChange', 'Tempo', 'PitchBend', 'TextMeta',
     'Track', 'Score'
