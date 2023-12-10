8e72564bef7c9a794adfd39d8b38d46ca3e63c8a
Yikai Liao <lyk-boya@outlook.com>
2023-12-10 16:46:57 +0800

    add numpy back

--- setup.py
+++ setup.py
@@ -143,8 +143,7 @@ setup(
     cmdclass={"build_ext": CMakeBuild},
     package_data={"symusic": ["**/*.pyi"]},
     zip_safe=False,
-    # install_requires=["numpy>=1.20", 'typing_extensions;python_version<"3.8"'],
-    install_requires=['typing_extensions;python_version<"3.8"'],    # don't need numpy currently
+    install_requires=["numpy"],
     setup_requires=["pybind11>=2.10"],
     python_requires=">=3.7",
     #extras_require={"test": ["pytest>=6.0"]},
