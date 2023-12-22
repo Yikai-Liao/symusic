d4696fe701e068dd30f25a534cada2c4e1bd02f6
Luo Zhongqi <luozhongqi@mail.com>
2023-12-22 15:51:01 +0800

    Modify generate_stub.sh for new building backend

--- generate_stub.sh
+++ generate_stub.sh
@@ -1,6 +1,5 @@
 rm -rf dist build *.egg-info
-python3 setup.py build bdist_wheel
-pip install dist/*.whl
+python3 -m pip install -e . --upgrade
 pybind11-stubgen symusic.core --numpy-array-remove-parameters -o .
 pip uninstall -y symusic
 rm -rf dist build *.egg-info
