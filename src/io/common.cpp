a401104c8a665b41e8eb2962a04fb5e5fb022b22
lyk <lyk-boya@outlook.com>
2023-12-31 14:39:12 +0800

    add fast_io to support utf8 path

--- src/io/common.cpp
+++ src/io/common.cpp
@@ -1,9 +1,9 @@
 //
 // Created by lyk on 23-12-25.
 //
-#include <cstdio>
-#include <stdexcept>
 
+#include <stdexcept>
+#include "fast_io_device.h"
 #include "symusic/io/common.h"
 
 #ifdef _MSC_VER
@@ -12,32 +12,23 @@
 
 namespace symusic {
 
-vec<u8> read_file(const std::filesystem::path& path) {
-    if(!exists(path)) { throw std::runtime_error("File not found"); }
-    FILE * file = fopen(path.string().c_str(), "rb");
-    if(!file) { throw std::runtime_error("File not found"); }
-    fseek(file, 0, SEEK_END);
-    const auto size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    vec<u8> buffer(size);
-    fread(buffer.data(), 1, size, file);
-    fclose(file);
+vec<u8> read_file(const std::u8string& path) {
+    fast_io::native_file_loader loader(fast_io::mnp::os_c_str(path));
+    vec<u8> buffer { loader.begin(), loader.end() };
     return buffer;
 }
 
-vec<u8> read_file(const std::string& path) {
-    return read_file(std::filesystem::path(path));
+vec<u8> read_file(const std::filesystem::path& path) {
+    return read_file(path.u8string());
+}
+
+void write_file(const std::u8string& path, const std::span<const u8> buffer) {
+    fast_io::obuf_file obf(fast_io::mnp::os_c_str(path));
+    fast_io::write(obf, buffer.begin(), buffer.end());
 }
 
 void write_file(const std::filesystem::path& path, const std::span<const u8> buffer) {
-    FILE * file = fopen(path.string().c_str(), "wb");
-    if(!file) { throw std::runtime_error("File not found"); }
-    fwrite(buffer.data(), 1, buffer.size(), file);
-    fclose(file);
-}
-
-void write_file(const std::string& path, const std::span<const u8> buffer) {
-    write_file(std::filesystem::path(path), buffer);
+    return write_file(path.u8string(), buffer);
 }
 
 } // namespace symusic
