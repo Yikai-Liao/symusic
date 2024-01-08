aa9d896dcc7de915aa4c5c553690b01ec663756f
lyk <lyk-boya@outlook.com>
2024-01-08 20:43:29 +0800

    add from_numpy to stub

--- generate_stub.sh
+++ generate_stub.sh
@@ -1,3 +1,5 @@
-pip install . --user
-pybind11-stubgen symusic.core --numpy-array-remove-parameters -o .
-pip uninstall symusic
+#pip install . --user
+nanobind-stubgen symusic.core --out .
+rm symusic/core.pyi
+mv symusic.core.pyi symusic/core.pyi
+#pip uninstall symusic
