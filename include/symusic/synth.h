0798db227496c5c6a5b8eb9cded3ce0390dce1e2
lyk <lyk-boya@outlook.com>
2024-02-12 08:00:19 +0800

    init synth

---
+++ include/symusic/synth.h
@@ -0,0 +1,26 @@
+//
+// Created by nhy on 2024/2/12.
+//
+#pragma once
+#ifndef SYNTH_H
+#define SYNTH_H
+
+#include "synthesizer.h"
+#include "symusic/score.h"
+
+
+namespace symusic {
+class Synthesizer {
+private:
+    psynth::Synthesizer synthesizer;
+
+public:
+    Synthesizer(const std::string &sfPath, const uint32_t sampleRate, const uint8_t quality):
+        synthesizer(sfPath, sampleRate, quality) {}
+
+    psynth::AudioData render_single_thread(const Score<Second> & score, bool stereo);
+    psynth::AudioData render(const Score<Second> & score, bool stereo, uint8_t workers);
+};
+
+}
+#endif //SYNTH_H
