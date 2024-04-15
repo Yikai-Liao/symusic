71b7ab27ae68459b2cc41e3c1ab09240928dfc94
lyk <lyk-boya@outlook.com>
2024-04-15 08:51:52 +0800

    bind filter

--- py_src/py_utils.h
+++ py_src/py_utils.h
@@ -210,7 +210,7 @@ auto bind_time_stamp(nb::module_& m, const std::string& name) {
         .def_prop_ro("ttype", [](const vec_t& self) { return typename T::ttype(); })
         .def("__getstate__", &vec_to_bytes<T>)
         .def("__setstate__", &vec_from_bytes<T>)
-        .def("sort", [](const vec_t& v, const nb::object & key, const bool reverse, const bool inplace) -> vec_t {
+        .def("sort", [](vec_t& v, const nb::object & key, const bool reverse, const bool inplace) -> vec_t {
             vec_t ans = inplace? v : std::make_shared<pyvec<T>>(std::move(v->copy()));
             if(key.is_none()) {
                 ans->sort(reverse, [](const auto& e) { return e.default_key(); });
@@ -219,7 +219,16 @@ auto bind_time_stamp(nb::module_& m, const std::string& name) {
             }
             return ans;
         },  nb::rv_policy::copy,
-        nb::arg("key") = nb::none(), nb::arg("reverse") = false, nb::arg("inplace") = true);
+        nb::arg("key") = nb::none(), nb::arg("reverse") = false, nb::arg("inplace") = true)
+        .def("filter", [](vec_t& v, const nb::object & func, const bool inplace) -> vec_t {
+            vec_t ans = inplace? v : std::make_shared<pyvec<T>>(std::move(v->copy()));
+            if(func.is_none()) {
+                throw std::invalid_argument("symusic::filter: You need to provide a function, not None!");
+            }
+            ans->filter(func);
+            return ans;
+        }, nb::rv_policy::copy,
+        nb::arg("func") = nb::none(), nb::arg("inplace") = true)
     ;
     // clang-format on
     return std::make_tuple(event, vec_class);
