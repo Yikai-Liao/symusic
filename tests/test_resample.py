977d22208ed27a08e795d4733a257b32b6f4e46d
lyk <lyk-boya@outlook.com>
2024-07-10 15:23:53 +0800

    add test files(to be implemented)

---
+++ tests/test_resample.py
@@ -0,0 +1,3 @@
+def test_resample():
+    # TODO: manually define some simple Scores and downsample them, check if symusic perform the right rounding
+    # TODO: upsample them, check if symusic perform the right interpolation
---
+++ tests/test_shift.py
@@ -0,0 +1,16 @@
+def test_shift_time():
+    # TODO: Load a test midi file and shift it by a random time, check if the time is shifted correctly
+    # check all container with shift_time function
+    ...
+
+
+def test_shift_pitch():
+    # TODO: Load a test midi file and shift it by a random pitch, check if the pitch is shifted correctly
+    # check all container with shift_pitch function
+    ...
+
+
+def test_shift_velocity():
+    # TODO: Load a test midi file and shift it by a random velocity, check if the velocity is shifted correctly
+    # check all container with shift_velocity function
+    ...
