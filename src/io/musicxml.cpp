49f4179da53380c4b2a2f174ae5b1aad1083388f
lyk <lyk-boya@outlook.com>
2025-01-26 21:23:17 +0800

    add time signature

--- src/io/musicxml.cpp
+++ src/io/musicxml.cpp
@@ -20,14 +20,18 @@ namespace symusic {
 
 namespace details {
 
-size_t expected_note_num(const minimx::Part& part) {
+inline size_t expected_note_num(const minimx::Part& part) {
     size_t num = 0;
     for (const auto& measure : part.measures) { num += measure.elements.size(); }
     return static_cast<size_t>(static_cast<float>(num) * 1.1);
 }
 
-double quarter_note_num(const minimx::Time& time) {
+inline double quarter_note_num(const minimx::Time& time) {
     return static_cast<double>(time.beats * 4) / time.beatType;
+}
+
+inline double quarter_note_num(const int beats, const int beatType) {
+    return static_cast<double>(beats * 4) / beatType;
 }
 
 vec<Note<Quarter>> parse_notes(const minimx::Part& part) {
@@ -68,21 +72,26 @@ vec<Note<Quarter>> parse_notes(const minimx::Part& part) {
                     if (tied_notes[pitch] == nullptr) {
                         throw std::runtime_error(
                             fmt::format(
-                                "symusic: Tied note does not exist at pitch {} when parsing musicxml.", pitch
+                                "symusic: Tied note does not exist at pitch {} when parsing "
+                                "musicxml.",
+                                pitch
                             )
                         );
                     }
-                    tied_notes[pitch]->duration = static_cast<f32>(cur_time - tied_notes[pitch]->time + duration);
-                    tied_notes[pitch]            = nullptr;
+                    tied_notes[pitch]->duration
+                        = static_cast<f32>(cur_time - tied_notes[pitch]->time + duration);
+                    tied_notes[pitch] = nullptr;
                 } else if (tied_notes[pitch] == nullptr) {
-                    // if tied_notes[pitch] is not nullptr, then this note is not in the middle of a tie
+                    // if tied_notes[pitch] is not nullptr, then this note is not in the middle of a
+                    // tie
                     if (element.actualNotes == 1) [[likely]] {
                         notes.emplace_back(cur_time, duration, pitch, 100);
                     } else {
                         // Deal with triplets or other time modifications
                         f64 duration_per_note = duration / element.actualNotes;
                         f64 tmp_time          = cur_time;
-                        for (int i = 0; i < element.actualNotes; ++i, tmp_time += duration_per_note) {
+                        for (int i = 0; i < element.actualNotes;
+                             ++i, tmp_time += duration_per_note) {
                             notes.emplace_back(tmp_time, duration_per_note, pitch, 100);
                         }
                     }
@@ -95,10 +104,13 @@ vec<Note<Quarter>> parse_notes(const minimx::Part& part) {
                     if (tied_notes[pitch] != nullptr) {
                         throw std::runtime_error(
                             fmt::format(
-                                "symusic: Tied note already exists at pitch {} when parsing musicxml.", pitch
+                                "symusic: Tied note already exists at pitch {} when parsing "
+                                "musicxml.",
+                                pitch
                             )
                         );
-                    }   tied_notes[pitch] = &notes.back();
+                    }
+                    tied_notes[pitch] = &notes.back();
                 }
                 break;
             }
@@ -117,11 +129,60 @@ vec<Note<Quarter>> parse_notes(const minimx::Part& part) {
     return notes;
 }
 
+vec<TimeSignature<Quarter>> build_time_signature(const minimx::MXScore& mx_score) {
+    if (mx_score.parts.empty()) {
+        throw std::runtime_error("symusic: Cannot check the time signature of an empty score.");
+    }
+    size_t measure_num = 0;
+    for (const auto& part : mx_score.parts) {
+        measure_num = std::max(measure_num, part.measures.size());
+    }
+
+    int pre_beats     = 0;
+    int pre_beat_type = 0;
+    double cur_time = 0;
+    vec<TimeSignature<Quarter>> time_signatures{};
+
+    for (auto i = 0; i < measure_num; ++i) {
+        int  cur_beats     = 0;
+        int  cur_beat_type = 0;
+        bool first         = true;
+        for (const auto& part : mx_score.parts) {
+            if (part.measures.size() <= i) { continue; }
+            const auto& measure = part.measures[i];
+            if (first) {
+                cur_beats     = measure.attributes.time.beats;
+                cur_beat_type = measure.attributes.time.beatType;
+                first         = false;
+            } else {
+                if (cur_beats != measure.attributes.time.beats
+                    || cur_beat_type != measure.attributes.time.beatType) {
+                    throw std::runtime_error(
+                        "symusic: Time signature mismatch in the MusicXML Score."
+                    );
+                }
+            }
+        }
+
+        if (cur_beats != pre_beats || cur_beat_type != pre_beat_type) {
+            time_signatures.emplace_back(cur_time, cur_beats, cur_beat_type);
+            pre_beats     = cur_beats;
+            pre_beat_type = cur_beat_type;
+        }
+        cur_time += quarter_note_num(cur_beats, cur_beat_type);
+    }
+    return time_signatures;
+}
+
 ScoreNative<Quarter> parse_musicxml_native(const pugi::xml_document& doc) {
     // Parsing the MusicXML file
     const minimx::MXScore mx_score{doc};
-    // Create a empty score
+    // Create an empty score
     ScoreNative<Quarter> score{};
+
+    if (mx_score.parts.empty()) { return score; }
+
+    score.time_signatures = std::move(build_time_signature(mx_score));
 
     score.tracks.reserve(mx_score.parts.size());
     for (const auto& part : mx_score.parts) {
