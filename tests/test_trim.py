e19d3e6204862e3a27eaf557d274ab91c1c45762
LUO Zhongqi <luozhongqi@mail.com>
2025-04-10 21:34:36 +0800

    Add a test

--- tests/test_trim.py
+++ tests/test_trim.py
@@ -1,4 +1,5 @@
 from symusic import Note, Score, Track
+from symusic.core import NoteQuarterList
 
 
 def test_trim_notes():
@@ -12,7 +13,9 @@ def test_trim_notes():
         Note(time=4, duration=1, pitch=72, velocity=100, ttype='Quarter')], ttype='Quarter')
 
     track = track.trim(start=1, end=3, min_overlap=0.1, start_mode='retain', end_mode='truncate')
-    print(track.notes)
+    trimmed = NoteQuarterList([Note(0.00, 3.00, 72, 100, 'Quarter'),
+                                Note(1.00, 1.00, 72, 100, 'Quarter'),
+                                Note(1.00, 2.00, 72, 100, 'Quarter'),
+                                Note(2.00, 1.00, 72, 100, 'Quarter')])
+    assert track.notes == trimmed
 
-
-test_trim_notes()
